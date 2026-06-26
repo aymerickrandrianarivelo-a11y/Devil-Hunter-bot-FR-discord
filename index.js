b7642230c6055ee5308d9812f98fcf7fd5067c65
aymerickrandrianarivelo-a11y <aymerick.randrianarivelo@gmail.com>
2026-06-26 09:51:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 import './src/app.js';
+// Empêche le bot de s'éteindre en cas d'erreur non capturée
+process.on('unhandledRejection', (reason, promise) => {
+    console.error(' [ERREUR REJETÉE] :', reason);
+});
+
+process.on('uncaughtException', (err, origin) => {
+    console.error(' [EXCEPTION NON CAPTURÉE] :', err);
+});
